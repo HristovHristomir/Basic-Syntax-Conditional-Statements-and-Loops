beea0f97ff25ffdbea2d8118adac73429b5260a2
Hristomir Hristov <58257470+HristovHristomir@users.noreply.github.com>
2022-07-21 21:16:38 +0300

    Create word_synonyms.py

---
+++ Dictionary Lab/word_synonyms.py
@@ -0,0 +1,14 @@
+n = int(input())
+synonyms_dict = {}
+last_key = ''
+
+for i in range(n):
+    word = input()
+    synonym = input()
+
+    if word not in synonyms_dict:
+        synonyms_dict[word] = []
+    synonyms_dict[word].append(synonym)
+
+for word in synonyms_dict:
+    print(f"{word} - {', '.join(synonyms_dict[word])}")
